bdeb5eeec0df6e88876fae3d2bc14db953be1331
classmac <classmac@SIW-MAC-C021.local>
2024-12-16 14:07:38 +0900

    test project adding

---
+++ 1216-test/1216-test/_216_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  _216_testApp.swift
+//  1216-test
+//
+//  Created by classmac on 2024/12/16.
+//
+
+import SwiftUI
+
+@main
+struct _216_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
